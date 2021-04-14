d582fcc9aadd316ec0930c964b793f83e08e01e6
Firebolt <24422634+Fireboltofdeath@users.noreply.github.com>
2021-04-14 18:54:28 -0400

    Expose Visualizer class

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import Object from "@rbxts/object-utils";
 import { RunService, Workspace } from "@rbxts/services";
 import { ConfigureSettings, HandleAdornments, Swappable } from "types";
 
-export namespace Visualize {
-	const swappableArrays = new Array<Swappable>();
-	const config: ConfigureSettings = {
+export class Visualizer {
+	private config: ConfigureSettings = {
 		enabled: true,
 		color: new Color3(1, 1, 1),
 		alwaysOnTop: true,
@@ -18,11 +17,16 @@ export namespace Visualize {
 		vectorLine: false,
 	};
 
-	function createHandleAdornment<T extends HandleAdornments>(className: T) {
+	private swappable = new Array<Swappable>();
+	private vectors = this.createSwappable<ConeHandleAdornment | CylinderHandleAdornment>();
+	private points = this.createSwappable<SphereHandleAdornment>();
+	private lines = this.createSwappable<CylinderHandleAdornment>();
+
+	private createHandleAdornment<T extends HandleAdornments>(className: T) {
 		const instance = new Instance(className);
-		instance.Transparency = config.transparency;
-		instance.AlwaysOnTop = config.alwaysOnTop;
-		instance.Color3 = config.color;
+		instance.Transparency = this.config.transparency;
+		instance.AlwaysOnTop = this.config.alwaysOnTop;
+		instance.Color3 = this.config.color;
 		instance.Visible = true;
 		instance.ZIndex = 2;
 		instance.Adornee = Workspace.Terrain;
@@ -30,29 +34,49 @@ export namespace Visualize {
 		return instance;
 	}
 
-	function createSwappable<T extends Instance>() {
+	private createSwappable<T extends Instance>() {
 		const swappable = {
 			used: new Array<T>(),
 			unused: new Array<T>(),
 		};
 
-		swappableArrays.push(swappable);
+		this.swappable.push(swappable);
 		return swappable;
 	}
 
-	function pop<T extends Instance>(swappable: Swappable<T>) {
-		return config.cacheAdornments ? swappable.unused.pop() : undefined;
+	private pop<T extends Instance>(swappable: Swappable<T>) {
+		return this.config.cacheAdornments ? swappable.unused.pop() : undefined;
 	}
 
-	const vectors = createSwappable<ConeHandleAdornment | CylinderHandleAdornment>();
-	const points = createSwappable<SphereHandleAdornment>();
-	const lines = createSwappable<CylinderHandleAdornment>();
+	private step() {
+		for (const swappable of this.swappable) {
+			for (const unused of swappable.unused) {
+				unused.Destroy();
+			}
+
+			swappable.unused = swappable.used;
+			swappable.used = [];
+		}
+	}
 
 	/**
-	 * Override the default Visualize settings.
+	 * A utility class for drawing different datatypes in 3d space.
 	 */
-	export function configure(newConfig: Partial<ConfigureSettings>) {
-		Object.assign(config, newConfig);
+	constructor(settings?: Partial<ConfigureSettings>) {
+		if (settings) this.configure(settings);
+
+		if (RunService.IsServer()) {
+			RunService.Heartbeat.Connect(() => this.step());
+		} else {
+			RunService.RenderStepped.Connect(() => this.step());
+		}
+	}
+
+	/**
+	 * Override the default Visualizer settings.
+	 */
+	configure(settings: Partial<ConfigureSettings>) {
+		Object.assign(this.config, settings);
 	}
 
 	/**
@@ -61,22 +85,23 @@ export namespace Visualize {
 	 * @param direction The direction of the vector.
 	 * @param color An optional color.
 	 */
-	export function vector(origin: Vector3, direction: Vector3, color = config.color) {
-		if (!config.enabled) return;
+	vector(origin: Vector3, direction: Vector3, color = this.config.color) {
+		if (!this.config.enabled || !Visualize.config.enabled) return;
 
 		let offset = 0;
-		let adornment = pop(vectors);
+		let adornment = this.pop(this.vectors);
 		if (!adornment) {
-			adornment = createHandleAdornment(config.vectorLine ? "CylinderHandleAdornment" : "ConeHandleAdornment");
+			const adornmentType = this.config.vectorLine ? "CylinderHandleAdornment" : "ConeHandleAdornment";
+			adornment = this.createHandleAdornment(adornmentType);
 			adornment.Height = math.max(direction.Magnitude, 1);
-			adornment.Radius = config.vectorRadius;
-			offset = config.vectorLine ? direction.Magnitude / 2 : 0;
+			adornment.Radius = this.config.vectorRadius;
+			offset = this.config.vectorLine ? direction.Magnitude / 2 : 0;
 		}
 
 		adornment.CFrame = CFrame.lookAt(origin, origin.add(direction)).mul(new CFrame(0, 0, -offset));
 		adornment.Color3 = color;
 
-		vectors.used.push(adornment);
+		this.vectors.used.push(adornment);
 	}
 
 	/**
@@ -84,19 +109,19 @@ export namespace Visualize {
 	 * @param origin The point's location.
 	 * @param color An optional color.
 	 */
-	export function point(origin: Vector3, color = config.color) {
-		if (!config.enabled) return;
+	point(origin: Vector3, color = this.config.color) {
+		if (!this.config.enabled || !Visualize.config.enabled) return;
 
-		let adornment = pop(points);
+		let adornment = this.pop(this.points);
 		if (!adornment) {
-			adornment = createHandleAdornment("SphereHandleAdornment");
-			adornment.Radius = config.pointRadius;
+			adornment = this.createHandleAdornment("SphereHandleAdornment");
+			adornment.Radius = this.config.pointRadius;
 		}
 
 		adornment.CFrame = new CFrame(origin);
 		adornment.Color3 = color;
 
-		points.used.push(adornment);
+		this.points.used.push(adornment);
 	}
 
 	/**
@@ -105,21 +130,21 @@ export namespace Visualize {
 	 * @param finish The end of the line.
 	 * @param color An optional color.
 	 */
-	export function line(start: Vector3, finish: Vector3, color = config.color) {
-		if (!config.enabled) return;
+	line(start: Vector3, finish: Vector3, color = this.config.color) {
+		if (!this.config.enabled || !Visualize.config.enabled) return;
 
-		let adornment = pop(lines);
+		let adornment = this.pop(this.lines);
 		if (!adornment) {
-			adornment = createHandleAdornment("CylinderHandleAdornment");
+			adornment = this.createHandleAdornment("CylinderHandleAdornment");
 			adornment.Height = start.sub(finish).Magnitude;
-			adornment.Radius = config.lineRadius;
-			adornment.InnerRadius = config.lineInnerRadius;
+			adornment.Radius = this.config.lineRadius;
+			adornment.InnerRadius = this.config.lineInnerRadius;
 		}
 
 		adornment.CFrame = CFrame.lookAt(start, finish).mul(new CFrame(0, 0, -start.sub(finish).Magnitude / 2));
 		adornment.Color3 = color;
 
-		lines.used.push(adornment);
+		this.lines.used.push(adornment);
 	}
 
 	/**
@@ -128,18 +153,14 @@ export namespace Visualize {
 	 * @param cframe The CFrame to render.
 	 * @param color An optional color.
 	 */
-	export function cframe(cframe: CFrame, color = config.color) {
-		vector(cframe.Position, cframe.LookVector.mul(config.cframeLength), color);
+	cframe(cframe: CFrame, color = this.config.color) {
+		this.vector(cframe.Position, cframe.LookVector.mul(this.config.cframeLength), color);
 	}
-
-	RunService.Heartbeat.Connect(() => {
-		for (const swappable of swappableArrays) {
-			for (const unused of swappable.unused) {
-				unused.Destroy();
-			}
-
-			swappable.unused = swappable.used;
-			swappable.used = [];
-		}
-	});
 }
+
+/**
+ * A global visualizer instance for convenience.
+ *
+ * Disabling the global visualizer will disable all Visualizer instances.
+ */
+export const Visualize = new Visualizer();
